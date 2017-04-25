79d085b5c71d08efd7d1327de3c872307a620fbf
James Sumners <james.sumners@gmail.com>
2017-04-25 11:11:41 -0400

    Damn npm skipping .gitignore

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const fs = require('fs')
 const path = require('path')
 const pwd = process.env['PWD']
 const files = [
-  path.join(__dirname, 'files', '.editorconfig'),
-  path.join(__dirname, 'files', '.gitignore')
+  path.join(__dirname, 'files', 'editorconfig'),
+  path.join(__dirname, 'files', 'gitignore')
 ]
 
 files.forEach((f) => {
   const fname = path.basename(f)
-  const outs = fs.createWriteStream(path.join(pwd, fname))
+  const outs = fs.createWriteStream(path.join(pwd, '.' + fname))
   const ins = fs.createReadStream(f)
   ins.pipe(outs)
 })
